34992f1655b19a3aedae9e1b54ad35e28e52df56
Vitaliy Kovalenko <vitalykolsky@gmail.com>
2021-01-29 11:48:48 +0300

    threads limit

--- src/main/java/ru/netris/camcorders/controllers/CamcorderRestController.java
+++ src/main/java/ru/netris/camcorders/controllers/CamcorderRestController.java
@@ -10,6 +10,7 @@ import java.util.concurrent.Future;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,54 +25,61 @@ import ru.netris.camcorders.services.CamcorderService;
  */
 @RestController
 public class CamcorderRestController {
-    
+
     private static Logger log = LogManager.getLogger(CamcorderRestController.class);
-    
+
     @Autowired
     private CamcorderService camcorderService;
-    
+
+    @Value("${camcorders.pool-max-size}")
+    private int poolMaxSize;
+
     @GetMapping("/")
     public Camcorder[] camcorders() {
-	Camcorder[] camcorders = camcorderService.fetchList();
-	
-	if (camcorders == null) {
-	    return null;
-	}
-	
-	List<Callable<Boolean>> tasks = new ArrayList<Callable<Boolean>>();
-	
-	for (Camcorder camcorder : camcorders) {
-	    tasks.add(() -> camcorderService.aggregateCamcorderData(camcorder));
-	}
-	
 	int completed = 0;
 	int successfully = 0;
 	int failed = 0;
-	
-	ExecutorService executor = Executors.newFixedThreadPool(tasks.size());
-	
-	try {
-	    List<Future<Boolean>> results = executor.invokeAll(tasks);
+	Camcorder[] camcorders = camcorderService.fetchList();
+
+	// The exception has already been thrown and logged
+	if (camcorders == null) {
+	    return null;
+	}
+
+	List<Callable<Boolean>> tasks = new ArrayList<Callable<Boolean>>();
+
+	for (Camcorder camcorder : camcorders) {
+	    tasks.add(() -> camcorderService.aggregateCamcorderData(camcorder));
 	    
-	    for (Future<Boolean> future : results) {
-		completed ++;
-		
-		if (future.get().booleanValue()) {
-		    successfully ++;
-		} else {
-		    failed ++;
+	    // Please edit properties if you want to run more threads.
+	    // But there must be a limit to avoid running a billion threads.
+	    if (tasks.size() == poolMaxSize || completed == camcorders.length - 1) {
+		ExecutorService executor = Executors.newFixedThreadPool(tasks.size());
+
+		try {
+		    List<Future<Boolean>> results = executor.invokeAll(tasks);
+
+		    for (Future<Boolean> future : results) {
+			if (future.get().booleanValue()) {
+			    successfully++;
+			} else {
+			    failed++;
+			}
+		    }
+		} catch (Exception e) {
+		    log.error(e);
+		} finally {
+		    executor.shutdown();
 		}
+		
+		tasks.clear();
 	    }
 	    
-	} catch (Exception e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	} finally {
-	    executor.shutdown();
+	    completed ++;
 	}
-	
-	log.info("Completed " + completed +  ", Successfully " + successfully + ", Failed " + failed);
-	
+
+	log.info("Completed " + completed + ", Successfully " + successfully + ", Failed " + failed);
+
 	return camcorders;
     }
 
